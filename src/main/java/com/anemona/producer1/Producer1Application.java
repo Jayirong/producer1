75250b94adb83162d4d448143c6a4f6afe54e612
Jayirong <lemonsoft5555@gmail.com>
2025-01-26 03:12:14 -0300

    first commit

---
+++ src/main/java/com/anemona/producer1/Producer1Application.java
@@ -0,0 +1,13 @@
+package com.anemona.producer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Producer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Producer1Application.class, args);
+	}
+
+}
